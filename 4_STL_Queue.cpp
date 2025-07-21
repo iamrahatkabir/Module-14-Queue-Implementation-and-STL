0650d4db714eb4f45ee2a4470000defef25842c8
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-21 20:02:27 +0600

    STL Queue

---
+++ 4_STL_Queue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    while(!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+
+
+    return 0;
+}
